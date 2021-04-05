c7ec9d8207bf58a455578f26292a0b891348efc5
Nishant  Shandilya <76595059+Nishant2020-coder@users.noreply.github.com>
2021-04-05 23:56:24 +0530

    Add files via upload

---
+++ nishant python.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+#Arithmetic operations
+
+
+# In[14]:
+
+
+#Addition
+234+24
+
+
+# In[15]:
+
+
+#Substraction
+324-23
+
+
+# In[16]:
+
+
+#Multiplication
+32*23
+
+
+# In[17]:
+
+
+#Division
+7/2
+
+
+# In[18]:
+
+
+#Floor division
+7//2
+
+
+# In[19]:
+
+
+#Mod
+7%2
+
+
+# In[20]:
+
+
+#Power
+3**2
+
+
+# In[21]:
+
+
+#Root
+64**0.5
+
+
+# In[22]:
+
+
+#Assign
+rate=200
+rate
+
+
+# In[23]:
+
+
+#Reassign
+rate=300
+rate
+
+
+# In[24]:
+
+
+# Compound Interest
+p=1000
+rate=10
+time=3
+ci=p*((1+(rate/100.0))**time)
+ci
+
